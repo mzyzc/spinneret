eafa46283cf9338484f6650bbeb6dc46584fec2c
mzyzc <mzyzc@czyz.xyz>
2020-10-20 22:28:28 +0100

    Moved command-line arguments to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
+mod options;
+
 use std::io::prelude::*;
 use std::fs;
 use std::net::{TcpListener, TcpStream};
-use clap::{Arg, App};
 
 fn main() -> std::io::Result<()> {
-    let matches = get_args();
+    let opts = options::Options::new();
 
-    let address = matches.value_of("address").unwrap_or("127.0.0.1:80");
-    let config = matches.value_of("config").unwrap_or("settings.conf");
-    let webroot = matches.value_of("webroot").unwrap_or("../www/index.html");
-    println!("Serving on address: {}", address);
-    println!("Config file located at: {}", config);
-    println!("Web root directory located at: {}", webroot);
-
-    let listener = TcpListener::bind(address)?;
+    let listener = TcpListener::bind(&opts.address)?;
 
     for stream in listener.incoming() {
-        handle_client(stream?, &matches);
+        handle_client(stream?, &opts);
     }
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream, args: &clap::ArgMatches) {
+fn handle_client(mut stream: TcpStream, opts: &options::Options) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
@@ -29,10 +23,9 @@ fn handle_client(mut stream: TcpStream, args: &clap::ArgMatches) {
 
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut request = httparse::Request::new(&mut headers);
-    let req_status = request.parse(&buffer).unwrap();
+    let _req_status = request.parse(&buffer).unwrap();
 
-    let webroot = args.value_of("webroot").unwrap_or("../www/index.html");
-    let html = fs::read_to_string(webroot).unwrap();
+    let html = fs::read_to_string(&opts.root_path).unwrap();
 
     let response = format!(
         "HTTP/1.1 200 OK\r\n\
@@ -47,27 +40,4 @@ fn handle_client(mut stream: TcpStream, args: &clap::ArgMatches) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-}
-
-fn get_args() -> clap::ArgMatches {
-    App::new("spinneret")
-        .arg(Arg::with_name("address")
-            .short('a')
-            .long("address")
-            .value_name("ADDRESS")
-            .takes_value(true)
-            .about("Sets address to serve on"))
-        .arg(Arg::with_name("config")
-            .short('c')
-            .long("config")
-            .value_name("FILE")
-            .takes_value(true)
-            .about("Sets config file"))
-        .arg(Arg::with_name("webroot")
-            .short('r')
-            .long("root")
-            .value_name("DIRECTORY")
-            .takes_value(true)
-            .about("Sets the web root directory"))
-        .get_matches()
 }
---
+++ src/options.rs
@@ -0,0 +1,72 @@
+use clap::{Arg, App};
+
+pub struct Options {
+    pub address: String,
+    pub config_path: String,
+    pub root_path: String,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options {
+            address: String::from("127.0.0.1:80"),
+            config_path: String::from("settings.conf"),
+            root_path: String::from("../www/index.html"),
+        }
+    }
+}
+
+impl Options {
+    pub fn new() -> Self {
+        let mut opts: Options = Default::default();
+        Self::set_args(&mut opts);
+        Self::print_options(&opts);
+
+        opts
+    }
+
+    fn set_args(opts: &mut Options) {
+        let args = Self::get_args();
+
+        if args.value_of("address").is_some() {
+            opts.address = String::from(args.value_of("address").unwrap());
+        }
+
+        if args.value_of("config").is_some() {
+            opts.config_path = String::from(args.value_of("config").unwrap());
+        }
+
+        if args.value_of("webroot").is_some() {
+            opts.root_path = String::from(args.value_of("webroot").unwrap());
+        }
+    }
+
+    fn get_args() -> clap::ArgMatches {
+        App::new("spinneret")
+            .arg(Arg::with_name("address")
+                .short('a')
+                .long("address")
+                .value_name("ADDRESS")
+                .takes_value(true)
+                .about("Sets address to serve on"))
+            .arg(Arg::with_name("config")
+                .short('c')
+                .long("config")
+                .value_name("FILE")
+                .takes_value(true)
+                .about("Sets config file"))
+            .arg(Arg::with_name("webroot")
+                .short('r')
+                .long("root")
+                .value_name("DIRECTORY")
+                .takes_value(true)
+                .about("Sets the web root directory"))
+            .get_matches()
+    }
+
+    fn print_options(opts: &Options) {
+        println!("Serving on address: {}", opts.address);
+        println!("Config file located at: {}", opts.config_path);
+        println!("Web root directory located at: {}", opts.root_path);
+    }
+}
